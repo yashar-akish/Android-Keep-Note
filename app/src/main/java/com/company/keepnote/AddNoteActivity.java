f75e1f451f005b97304607849b4e25a9a8d3e40a
yashar-akish <yashar_akish@yahoo.com>
2021-05-23 17:53:31 -0400

    preparing addNoteActivity for adding to db

--- app/src/main/java/com/company/keepnote/AddNoteActivity.java
+++ app/src/main/java/com/company/keepnote/AddNoteActivity.java
@@ -1,14 +1,48 @@
 package com.company.keepnote;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class AddNoteActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class AddNoteActivity extends AppCompatActivity {
+    EditText titleET, descriptionET;
+    Button saveBTN, cancelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Add Note");
         setContentView(R.layout.activity_add_note);
+
+        titleET = findViewById(R.id.titleET);
+        descriptionET = findViewById(R.id.descriptionET);
+        saveBTN = findViewById(R.id.saveBTN);
+        cancelBtn = findViewById(R.id.cancelBTN);
+
+        saveBTN.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.saveBTN) {
+            saveNote();
+        } else if (v.getId() == R.id.cancelBTN) {
+            Toast.makeText(AddNoteActivity.this,"Nothing Saved", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    public void saveNote() {
+        String noteTitle = titleET.getText().toString();
+        String noteDescription = descriptionET.getText().toString();
+
+
     }
 }
