e6faf1689978d9f6a67e434c030df9b6fd7553ef
yashar-akish <yashar_akish@yahoo.com>
2021-05-23 14:55:41 -0400

    adding the neccessary codes for recyclerView

--- app/src/main/java/com/company/keepnote/MainActivity.java
+++ app/src/main/java/com/company/keepnote/MainActivity.java
@@ -3,6 +3,8 @@ package com.company.keepnote;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -17,12 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication())
                 .create(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 //update the recyclerView
+                adapter.setNotes(notes);
             }
         });
     }
---
+++ app/src/main/java/com/company/keepnote/NoteAdapter.java
@@ -0,0 +1,58 @@
+package com.company.keepnote;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<>();
+
+    public void setNotes(List<Note> notes) {
+
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteAdapter.NoteHolder holder, int position) {
+
+        Note currentNote = notes.get(position);
+        holder.titleTV.setText(currentNote.getTitle());
+        holder.descriptionTv.setText(currentNote.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+
+        TextView titleTV, descriptionTv;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleTV = itemView.findViewById(R.id.titleTV);
+            descriptionTv = itemView.findViewById(R.id.descriptionTV);
+        }
+    }
+
+
+}
