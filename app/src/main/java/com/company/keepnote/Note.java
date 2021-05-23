32eef0cba18a57322ca226898aacae196b110bd6
yashar-akish <yashar_akish@yahoo.com>
2021-05-22 22:29:12 -0400

    creating Entity (Note class)

---
+++ app/src/main/java/com/company/keepnote/Note.java
@@ -0,0 +1,40 @@
+package com.company.keepnote;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String title;
+
+    public String description;
+
+    //constructor
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    //setter
+    public void setId(int id) {
+        this.id = id;
+    }
+}
